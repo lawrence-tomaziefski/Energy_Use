507027eb7fc3e994cad40dc026b357a2f6f311b4
Lawrence Tomaziefski <lawrence.tomaziefski@gmail.com>
2016-12-03 11:04:26 -0600

    Test

---
+++ plot1.R
@@ -0,0 +1,41 @@
+#Set working directory and folder to store data
+setwd('/Users/lawrence_tomaziefski/R_Working_Directory/161130_Exploratory_Data_Analysis')
+dir.create("./energy_use")
+path = ("./energy_use/")
+require(dplyr)
+require(data.table)
+require(lubridate)
+
+household_power = read.table(paste0(path,"household_power_consumption.txt"),
+                             header = TRUE, 
+                             na.strings = "NA", 
+                             sep = ";",
+                             dec = ".",
+                             stringsAsFactors = FALSE)
+household_power = mutate(household_power,
+                         datetime = dmy_hms(paste(household_power$Date,household_power$Time)))
+household_power = mutate(household_power,Date = dmy(household_power$Date))
+household_power = filter(household_power,Date == "2007-02-01" | Date == "2007-02-02")
+
+for(i in 3:9){
+        household_power[,i] = as.numeric(household_power[,i])
+}
+width = 480
+height = 480
+
+hist(household_power$Global_active_power,
+     col = "red", 
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylim = c(0,1200))
+dev.copy(png, file = "plot1.png",width = width, height = height)
+## Don't forget to close the PNG device!
+dev.off()
+
+     
+     
+
+
+
+
+
